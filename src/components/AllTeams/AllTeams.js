2c123a2ba32b99df2a7c6b1d44b9fee16b05b346
rasel <russelroy15@gmail.com>
2021-10-03 17:28:15 +0600

    WIP on sports world project

--- src/components/AllTeams/AllTeams.js
+++ src/components/AllTeams/AllTeams.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./AllTeams.css";
 const AllTeams = () => {
+  const [teams, setTeams] = useState([]);
+  useEffect(() => {
+    fetch("https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=Arsenal")
+      .then((res) => res.json())
+      .then((data) => setTeams(data.teams));
+  }, []);
   return (
     <div className="team-container">
-      <h1>Our Team</h1>
+      <h1>Our Team {teams.length}</h1>
       <div className="search-box">
         <input
           type="text"
