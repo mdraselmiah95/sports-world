8817b67898efc72a689cd2d4fb899c9df90ec218
rasel <russelroy15@gmail.com>
2021-10-03 19:58:16 +0600

    WIP on sports world project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { createContext, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -5,29 +6,32 @@ import Error from "./components/Error/Error";
 import Home from "./components/Home/Home";
 import Sports from "./components/Sports/Sports";
 import TeamDetails from "./components/TeamDetails/TeamDetails";
-
+const userContext = createContext();
 function App() {
+  const [user, setUser] = useState({});
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/sports">
-            <Sports />
-          </Route>
-          <Route path="/details/:teamId">
-            <TeamDetails />
-          </Route>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
-      </Router>
+      <userContext.Provider value={[user, setUser]}>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/sports">
+              <Sports />
+            </Route>
+            <Route path="/details/:teamId">
+              <TeamDetails />
+            </Route>
+            <Route path="*">
+              <Error />
+            </Route>
+          </Switch>
+        </Router>
+      </userContext.Provider>
     </div>
   );
 }
